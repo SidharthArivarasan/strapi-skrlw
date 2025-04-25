5b212ec0a8be904a34f7e0b263db10d1dedb5e0c
Sidharth Arivarasan <sidhartharivarasan@gmail.com>
2025-04-25 16:04:20 -0500

    Okta Integration - 1

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,23 @@
-export default () => ({});
+export default ({ env }) => ({
+    'users-permissions': {
+      config: {
+        providers: [
+          {
+            uid: 'okta',
+            displayName: 'Okta',
+            icon: 'https://okta.com/favicon.ico',
+            createStrategy: require('@strapi/plugin-users-permissions/server/providers/openid-connect'),
+            strategy: 'openid-connect',
+            params: {
+              issuer: env('OKTA_ISSUER'),
+              clientId: env('OKTA_CLIENT_ID'),
+              clientSecret: env('OKTA_CLIENT_SECRET'),
+              scope: ['openid', 'profile', 'email'],
+              state: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+  
