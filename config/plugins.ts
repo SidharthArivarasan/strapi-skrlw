f7a593c4794dc771dc9b41d66a9ed427f0bd7257
Sidharth Arivarasan <sidhartharivarasan@gmail.com>
2025-04-27 20:48:58 -0500

    Google SSO with Render check

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,22 @@
-export default () => ({});
+export default ({ env }) => ({
+    'users-permissions': {
+      config: {
+        providers: [
+          {
+            uid: 'google',
+            displayName: 'Google',
+            icon: 'https://cdn-icons-png.flaticon.com/512/2702/2702602.png',
+            createStrategy: require('@strapi/plugin-users-permissions/server/providers/google').default,
+            strategy: 'google',
+            params: {
+              clientId: env('GOOGLE_CLIENT_ID'),
+              clientSecret: env('GOOGLE_CLIENT_SECRET'),
+              scope: ['openid', 'profile', 'email'],
+              prompt: 'consent',
+            },
+          },
+        ],
+      },
+    },
+  });
+  
--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,9 @@
 export default ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1338),
+  url: 'http://localhost:1338',
   app: {
     keys: env.array('APP_KEYS'),
   },
+  proxy: true,
 });
